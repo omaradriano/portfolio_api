6eb65da31a21bac3cee4ec78daf2252a6ab360c1
Omar Adrian <oadrian38@gmail.com>
2024-02-09 02:55:35 -0600

    add route for add users
    Esto maneja la autenticacion por el lado del servidor

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import express, { Request, Response, NextFunction } from 'express'
+import bodyParser from 'body-parser'
+// import cors from 'cors'
+
+// Routes
+import signup from './routes/signup.routes'
+
+type WhiteList = Array<string>
+
+const PORT = 3001
+const whiteList: WhiteList = ["http://127.0.0.1:3000", "http://localhost:3000"]
+
+
+try {
+    const app = express()
+
+    //Body parser para el cuerpo de las solicitudes con json
+    app.use(bodyParser.json({
+        limit: '500kb'
+    }))
+
+    // Uso de los headers para permitir peticiones de los sitios especificados
+    app.use((req: Request, res: Response, next: NextFunction) => {
+        const origin: string | undefined = req.headers.origin;
+        if (origin && whiteList.includes(origin)) {
+            res.setHeader('Access-Control-Allow-Origin', origin);
+        }
+        res.append('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+        res.append('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        next();
+    });
+
+    // Rutas para el registro de los usuarios
+    app.use(signup)
+
+    app.listen(PORT, () => {
+        console.log(`Running server on port ${PORT}`);
+    })
+} catch (error) {
+    const err = (error as Error).message
+    console.log(err);
+}
+
---
+++ src/routes/signup.routes.ts
@@ -0,0 +1,77 @@
+import { Collection, User } from "discord.js";
+import express, { Request, Response, Router } from "express";
+import { MongoClient } from "mongodb";
+
+const signup = Router()
+const uri = 'mongodb://127.0.0.1:27017/portfolio_api'
+
+const dbname = 'portfolio_api';
+const dbcollection = 'user';
+
+// profile.get('/profile/:username', async (request, response) => {
+//     const mongo = await MongoClient.connect(uri)
+//     await mongo.connect()
+
+//     const { username } = request.params
+
+//     const BDD = mongo.db('portfolio_api');
+//     const coleccion = BDD.collection('user');
+
+//     // Insertar el documento en la colección
+//     const resultado = await coleccion.find({ "name": username }).toArray();
+
+//     response.send(resultado[0].name)
+//     console.log(resultado[0].name)
+//     await mongo.close()
+// })
+
+
+// Agregar un usuario
+
+interface UserData {
+    user: string
+    email: string
+    password: string
+}
+
+signup.post('/signup', async (req: Request, res: Response) => {
+    const client = await MongoClient.connect(uri)
+    const { user, email, password }: UserData = req.body
+    try {
+        const db = client.db(dbname)
+        const collection = db.collection(dbcollection)
+
+        const ifUserExists = await collection.findOne({ "user": user }).then(res => res)
+        if (ifUserExists){
+            console.log('Usuario ya existe');
+            return res.status(400).json({ message: 'El usuario ya existe' })
+        } 
+
+        const ifEmailExists = await collection.findOne({ "email": email }).then(res => res)
+        if (ifEmailExists){
+            console.log('Correo electronico ya registrado');
+            return res.status(400).json({ message: 'Ya existe un correo registrado' })
+        } 
+
+        const data: UserData = {
+            user, email, password
+        }
+
+        const result = await collection.insertOne(data);
+        console.log('Documento insertado con ID:', result.insertedId);
+
+        res.status(200).json({ message: 'Usuario registrado con exito' })
+        
+    } catch (err) {
+        console.error((err as Error).message);
+        res.status(500).json({ message: 'Error interno del servidor' });
+    } finally {
+        client.close();
+    }
+})
+
+
+
+
+export default signup
+
